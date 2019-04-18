3c2474dde816b50cf30f17213cfaf2a56c84e000
Emiliano Tortorella <emitor@gmail.com>
2019-04-18 09:49:08 -0500

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,8 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id("id_list_table")
-		rows = table.find_elements_by_tag_name("tr")
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
 
 
@@ -21,30 +21,30 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrive_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check its hompage 
-		self.browser.get("http://localhost:8000")
+		self.browser.get('http://localhost:8000')
 
 		# She notice the page title and header mention to-do lists
-		self.assertIn("To-Do", self.browser.title)
-		header_text = self.browser.find_element_by_tag_name("h1").text
-		self.assertIn("To-Do", header_text)
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# She is invited to enter a to-do item straight away
-		inputbox =  self.browser.find_element_by_id("id_new_item")
+		inputbox =  self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-			inputbox.get_attribute("placeholder"),
-			"Enter a to-do item"
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
 		)
 
 		# She types "Buy peacock feathers" into a textbox (Edith's hobby
 		# is tying fly-fishing lures)
-		inputbox.send_keys("Buy peacock feathers")
+		inputbox.send_keys('Buy peacock feathers')
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		self.check_for_row_in_list_table("1: Buy peacock feathers")
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text book inviting her to add another item. She
 		# enters "Use peackock feathers to make a fly" (Edith is very methodical)
@@ -54,13 +54,13 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# The page updates again, and shows both items on her list
-		self.check_for_row_in_list_table("1: Buy peacock feathers")
-		self.check_for_row_in_list_table("2: Use peackock feathers to make a fly")
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peackock feathers to make a fly')
 
 		# Edith wonder whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect
-		self.fail("Finish the test")
+		self.fail('Finish the test')
 
 		# She visits that URL - her to-do list is still there.
 
