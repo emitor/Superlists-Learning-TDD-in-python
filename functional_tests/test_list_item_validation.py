6b2267a9ada5f0e88ddd8587e7444d8b201d9cca
Emiliano Tortorella <emitor@gmail.com>
2019-04-25 15:44:43 -0500

    Some typos fixes on FT. Refactor hard-coded URLs out of templates

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,27 +12,27 @@ class ItemValidationTest(FunctionalTest):
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
 		self.wait_for( lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has_error').text,
+			self.browser.find_element_by_css_selector('.has-error').text,
 			"You can't have an empty list item"
 		))
 
 		# She tries again with some text for the item, wich now works
-		self.browser.find_element_by_id('id_new_item').sendKeys('Buy milk')
-		self.browser.find_element_by_id('id_new_item').sendKeys(Keys.ENTER)
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 
 		# Perversely, she now decides to submit a second blank list item
-		self.browser.find_element_by_id('id_new_item').sendKeys(Keys.ENTER)
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
 		# She receives a similar warning on the list page
 		self.wait_for( lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has_error').text,
+			self.browser.find_element_by_css_selector('.has-error').text,
 			"You can't have an empty list item"
 		))
 
 		# And she can correct it by filling some text in
-		self.browser.find_element_by_id('id_new_item').sendKeys('Make tea')
-		self.browser.find_element_by_id('id_new_item').sendKeys(Keys.ENTER)
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
 
